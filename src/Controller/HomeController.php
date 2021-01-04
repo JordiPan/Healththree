cad47df8eb57c5a30dc5772ce0a0a0bde5a29232
Jordipan <jordipan.jp@gmail.com>
2021-01-04 15:17:06 +0100

    tabel toegevoegd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,33 +15,5 @@ class HomeController extends AbstractController {
     public function showHome() {
         return $this->render('home.html.twig');
     }
-    /**
-     * @Route("door/{title}", name="commentSection")
-     */
-    public function show($title, EntityManagerInterface $em) {
-        $repository = $em->getRepository(Medicines::class);
-        $medicine = $repository->findOneBy(['id' => 1]);
 
-        if (!$medicine) {
-            throw $this->createNotFoundException(sprintf("no Medicine found"));
-        }
-
-        $comments = [
-            'why die?',
-            'just live',
-            'eat sht',
-            'I kill problems with knowledge'
-        ];
-        return $this->render('showComments.html.twig', [
-            'title' => ucwords(str_replace('-',' ', $title)),
-            'comments' => $comments,
-            'medicine' => $medicine
-        ]);
-    }
-    /**
-     * @Route("table")
-     */
-    public function showTable() {
-        return new Response("Bruh");
-    }
 }
--- src/Controller/MedicineAdminController.php
+++ src/Controller/MedicineAdminController.php
@@ -1,28 +1,34 @@
 <?php
 namespace App\Controller;
-
 use App\Entity\Medicines;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Form\MedicijnType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MedicineAdminController extends AbstractController{
+class MedicineAdminController extends AbstractController {
     /**
-     * @Route("/admin/medicine")
+     * @Route("/admin/medicine", name="medicine_maker")
      */
-    public function new(EntityManagerInterface $em) {
-        $medicine = new Medicines();
-        $medicine->setNaam("Ibuprofen");
-        $medicine->setWerking("Geen idee");
-        $medicine->setPrijs(1.99);
-        $medicine->setVerzekerd(1);
+    public function newMedicine(Request $request)
+    {
+        $medicijn=new Medicines();
+       $form = $this->createForm(MedicijnType::class, $medicijn);
 
-        $em->persist($medicine);
-        $em->flush();
-        return new Response(sprintf(
-            "regel aangemaakt in Medicines tabel. Regel: %d",
-        $medicine->getId()
-        ));
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($medicijn);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('table');
+        }
+
+
+
+        return $this->render('create.html.twig', [
+           'medicineForm' => $form->createView()
+       ]);
     }
 }
