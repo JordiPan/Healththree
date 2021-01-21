53ac51d201ec7a70c13c6fd7ee8c7c5e1a720555
JordiPan <jordipan.jp@gmail.com>
2021-01-21 11:44:12 +0100

    font awesome toegevoegd

---
+++ src/Entity/Patient.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PatientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PatientRepository::class)
+ */
+class Patient
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255 , nullable=true)
+     */
+    private $middleName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $emailAddress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Recept::class, mappedBy="patient")
+     */
+    private $recepts;
+
+    public function __construct()
+    {
+        $this->recepts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getMiddleName(): ?string
+    {
+        return $this->middleName;
+    }
+
+    public function setMiddleName(string $middleName): self
+    {
+        $this->middleName = $middleName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getEmailAddress(): ?string
+    {
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress(string $emailAddress): self
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Recept[]
+     */
+    public function getRecepts(): Collection
+    {
+        return $this->recepts;
+    }
+
+    public function addRecept(Recept $recept): self
+    {
+        if (!$this->recepts->contains($recept)) {
+            $this->recepts[] = $recept;
+            $recept->setPatient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecept(Recept $recept): self
+    {
+        if ($this->recepts->removeElement($recept)) {
+            // set the owning side to null (unless already changed)
+            if ($recept->getPatient() === $this) {
+                $recept->setPatient(null);
+            }
+        }
+
+        return $this;
+    }
+}
